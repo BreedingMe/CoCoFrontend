e4a2a4fa0675d17dbaf9f7c631ef0a5293b32ab9
kangwongu <kwg0527@naver.com>
2022-07-11 10:33:19 +0900

    Feat: Create email, nickname dup check

--- src/js/register-modal.js
+++ src/js/register-modal.js
@@ -83,14 +83,6 @@ window.requestRegister = () => {
         return;
     }
 
-    // 이메일이 중복이면
-    if (!isEmail(id)) {
-        $('#register-modal-input-id').addClass('is-danger');
-        $('#register-modal-input-id').focus();
-        $('#register-modal-help').text('중복된 이메일이 존재합니다.');
-        return;
-    }
-
     $('#register-modal-input-id').removeClass('is-danger');
     $('#register-modal-help').text('');
 
@@ -99,8 +91,7 @@ window.requestRegister = () => {
     if (password == '') {
         $('#register-modal-input-password').addClass('is-danger');
         $('#register-modal-input-password').focus();
-        $('#register-modal-help').text('비밀번호를 입력해 주세요.');
-
+        $('#register-modal-help').text('비밀번호를 입력해 주세요.').removeClass('is-success').addClass('is-danger');
         return;
     }
 
@@ -112,7 +103,7 @@ window.requestRegister = () => {
     if (confirmPassword == '') {
         $('#register-modal-input-confirm-password').addClass('is-danger');
         $('#register-modal-input-confirm-password').focus();
-        $('#register-modal-help').text('비밀번호 확인을 입력해 주세요.');
+        $('#register-modal-help').text('비밀번호 확인을 입력해 주세요.').removeClass('is-success').addClass('is-danger');;
 
         return;
     }
@@ -126,7 +117,7 @@ window.requestRegister = () => {
         $('#register-modal-input-password').addClass('is-danger');
         $('#register-modal-input-confirm-password').addClass('is-danger');
         $('#register-modal-input-password').focus();
-        $('#register-modal-help').text('비밀번호가 일치하지 않습니다.');
+        $('#register-modal-help').text('비밀번호가 일치하지 않습니다.').removeClass('is-success').addClass('is-danger');;
 
         return;
     }
@@ -134,7 +125,7 @@ window.requestRegister = () => {
     if (!isPassword(password)) {
         $('#register-modal-input-password').addClass('is-danger');
         $('#register-modal-input-password').focus();
-        $('#register-modal-help').text('영문과 숫자 조합의 8-20자의 비밀번호를 설정해주세요.');
+        $('#register-modal-help').text('영문과 숫자 조합의 8-20자의 비밀번호를 설정해주세요.').removeClass('is-success').addClass('is-danger');;
 
         return;
     }
@@ -148,16 +139,7 @@ window.requestRegister = () => {
     if (nickname == '') {
         $('#register-modal-input-nickname').addClass('is-danger');
         $('#register-modal-input-nickname').focus();
-        $('#register-modal-help').text('닉네임을 입력해 주세요.');
-
-        return;
-    }
-
-    // 닉네임이 중복이면
-    if (nickname == '') {
-        $('#register-modal-input-nickname').addClass('is-danger');
-        $('#register-modal-input-nickname').focus();
-        $('#register-modal-help').text('중복된 닉네임이 존재합니다.');
+        $('#register-modal-help').text('닉네임을 입력해 주세요.').removeClass('is-success').addClass('is-danger');;
 
         return;
     }
@@ -170,7 +152,7 @@ window.requestRegister = () => {
     if (admin == true && adminToken == '') {
         $('#register-modal-input-admin-token').addClass('is-danger');
         $('#register-modal-input-admin-token').focus();
-        $('#register-modal-help').text('관리자 토큰을 입력해 주세요.');
+        $('#register-modal-help').text('관리자 토큰을 입력해 주세요.').removeClass('is-success').addClass('is-danger');;
 
         return;
     }
@@ -204,4 +186,81 @@ function isEmail(asValue) {
 function isPassword(asValue) {
     const regExp = /^(?=.*\d)(?=.*[a-zA-Z])[0-9a-zA-Z!@#$%^&*]{8,20}$/;
     return regExp.test(asValue);
+}
+
+// 이메일 중복 확인
+window.checkEmailDup = () => {
+    let email = $('#register-modal-input-id').val();
+
+    if (email == '') {
+        $('#register-modal-input-id').addClass('is-danger');
+        $('#register-modal-input-id').focus();
+        $('#register-modal-help').text('이메일을 입력해 주세요.');
+
+        return;
+    }
+
+    if (!isEmail(email)) {
+        $('#register-modal-input-id').addClass('is-danger');
+        $('#register-modal-input-id').focus();
+        $('#register-modal-help').text('정확한 이메일 형식으로 입력해 주세요.');
+        return;
+    }
+
+    $.ajax({
+        type: 'POST',
+        url: process.env.BACKEND_HOST + '/register/check-email',
+        contentType: 'application/json',
+        data: JSON.stringify({
+            email: email
+        }),
+        success: function (response) {
+            if (response['dup']) {
+                $('#register-modal-input-id').addClass('is-danger');
+                $('#register-modal-input-id').focus();
+                $('#register-modal-help').text('중복된 이메일이 존재합니다.');
+                return;
+            } else {
+                $('#register-modal-input-id').removeClass('is-danger').addClass('is-safe');
+                $("#input-username").focus();
+                $('#register-modal-help').text('사용 가능한 이메일입니다.').removeClass('is-danger').addClass('is-success');
+            }
+        }
+    });
+}
+
+// 닉네임 중복 확인
+window.checkNicknameDup = () => {
+    let nickname = $('#register-modal-input-nickname').val();
+    console.log(nickname);
+
+    if (nickname == '') {
+        $('#register-modal-input-nickname').addClass('is-danger');
+        $('#register-modal-input-nickname').focus();
+        $('#register-modal-help').text('닉네임을 입력해 주세요.').removeClass('is-success').addClass('is-danger');;
+
+        return;
+    }
+
+    $.ajax({
+        type: 'POST',
+        url: process.env.BACKEND_HOST + '/register/check-nickname',
+        contentType: 'application/json',
+        data: JSON.stringify({
+            nickname: nickname
+        }),
+        success: function (response) {
+            if (response['dup']) {
+                $('#register-modal-input-nickname').addClass('is-danger');
+                $('#register-modal-input-nickname').focus();
+                $('#register-modal-help').text('중복된 닉네임이 존재합니다.').removeClass('is-success').addClass('is-danger');
+
+                return;
+            } else {
+                $('#register-modal-input-nickname').removeClass('is-danger').addClass('is-safe');
+                $("#register-modal-input-nickname").focus();
+                $('#register-modal-help').text('사용 가능한 닉네임입니다.').removeClass('is-danger').addClass('is-success');
+            }
+        }
+    });
 }
