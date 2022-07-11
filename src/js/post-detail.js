a8ad900cdecda8f7183e83cbf7a21baea154836c
rimsong <kariber21@naver.com>
2022-07-11 22:07:37 +0900

    Fix: Update Post enter output

--- src/js/post-detail.js
+++ src/js/post-detail.js
@@ -39,14 +39,54 @@ function getPost() {
             const time = new Date(date + '+0900').toTimeString().split(' ')[0];
             const datestr = day + ' ' + time;
 
-            $('#nickname_openPost').html(nickname);
-            $('#title_openPost').html(title);
-            $('#content_openPost').html(content);
-            $('#meetingType_openPost').html(meetingType);
-            $('#contact_openPost').html(contact);
-            $('#period_openPost').html(period);
-            $('#recruitmentState_openPost').html(recruitmentState);
-            $('#datestr_openPost').html(datestr);
+            let htmlTemp = `<div class="post-boundary">
+            <button id="deletebtn" class="button has-text-centered is-rounded deletebtn" onclick="deletePost()">삭제</button>
+            <button id="updatebtn" class="button has-text-centered is-rounded updatebtn" onclick="showPostUpdatePage()">수정</button>
+            <p><div style="font-size: xx-large; font-weight: bold;" id="title_openPost">${title}</div></p>
+                    <br>
+                        <span style="float: left; margin-left: 8px;" id="nickname_openPost">${nickname}</span>
+                        <div style="float: right">
+                            <small id="datestr_openPost">${datestr}</small>
+                        </div>
+                        <hr style="margin-top: 40px; margin-bottom: 10px;">
+                        <div class="post-page">
+                            <ul class="area">
+                                <l>
+                                    <span class="component">모집 여부</span>
+                                    <span id="recruitmentState_openPost">${recruitmentState}</span>
+                                </li>
+                                <li>
+                                    <span class="component">모임 방법</span>
+                                    <span id="meetingType_openPost">${meetingType}</span>
+                                </li>
+                                <li>
+                                    <span class="component">문의 방법</span>
+                                    <span id="contact_openPost">${contact}</span>
+                                </li>
+                                <li>
+                                    <span class="component">예상 기간</span>
+                                    <span id="period_openPost">${period}</span>
+                                </li>
+                            </ul>
+                            <hr style="margin-top: 2px;">
+                            <ul class="area">
+                                <li>
+                                    <span style="font-size: larger; font-weight: bold">프로젝트 내용</span>
+                                </li>
+                            </ul>
+                            <div id="content_openPost" style="white-space:pre">${content}</div>
+                        </div>
+                    </div>`;
+
+            $('#postingbox').append(htmlTemp);
+            // $('#nickname_openPost').html(nickname);
+            // $('#title_openPost').html(title);
+            // $('#content_openPost').html(content);
+            // $('#meetingType_openPost').html(meetingType);
+            // $('#contact_openPost').html(contact);
+            // $('#period_openPost').html(period);
+            // $('#recruitmentState_openPost').html(recruitmentState);
+            // $('#datestr_openPost').html(datestr);
 
             if (enableUpdate == false) {
                 $('#updatebtn').attr('class', 'button none');
