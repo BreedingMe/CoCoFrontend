f369463050c563901d7833b1fb6b1f7483aac5de
kangwongu <kwg0527@naver.com>
2022-07-18 20:03:08 +0900

    Feat: Create user's post list in profile page

--- src/js/profile.js
+++ src/js/profile.js
@@ -10,8 +10,14 @@ $(document).ready(() => {
 
 window.initializeProfile = () => {
     getProfile();
+    readMyPosts();
 };
 
+// window.initializePost = () => {
+//     getPost();
+//     window.getCommentList();
+//     //다른 js에서(코멘트) 불러오는 거라 넣어줌!
+// };
 /* JS */
 // 회원 정보 수정 열기 모달
 window.openEditProfileModal = () => {
@@ -376,3 +382,145 @@ window.checkNicknameDupProfile = () => {
 //     }
 //     $(`#${currentScreen}`).show();
 // }
+
+window.readMyPosts = () => {
+    $('#readMyPosts').attr('class', 'is-active');
+    $('#readMyComments').attr('class', '');
+    $('#profile-section').empty();
+
+    let token = Cookies.get('token');
+    $.ajax({
+        type: 'GET',
+        url: process.env.BACKEND_HOST + '/profile/posts',
+        beforeSend: function (xhr) {
+            xhr.setRequestHeader('Content-type', 'application/json');
+            xhr.setRequestHeader('Authorization', 'Bearer ' + token);
+        },
+        success: function (response) {
+            let posts = response;
+
+            for (let index = 0; index < posts.length; index++) {
+                let id = posts[index]['id'];
+                let title = posts[index]['title'];
+                let meetingType = posts[index]['meetingType'];
+                let period = posts[index]['period'];
+                let hits = posts[index]['hits'];
+                let recruitmentState = posts[index]['recruitmentState'] ? '모집 완료' : '모집 중';
+
+                let recruitmentStateColor = posts[index]['recruitmentState'] ? 'is-default' : 'is-pink';
+                let recruitmentStateColorBack = posts[index]['recruitmentState'] ? 'is-white' : 'is-gray';
+
+                let tempHTML = `<div id=${id} class="card ${recruitmentStateColorBack}">
+                                    <div class="card-header">
+                                        <p class="card-header-title" onclick="openPost(${id})" >${title}</p>
+                                        <div onclick="deleteBookmark(${id})">
+                                            <button class="delete"></button>
+                                        </div>
+                                    </div>
+
+                                    <div class="card-content" onclick="openPost(${id})">
+                                        <div class="card-content-box">
+                                            <div class="content">
+                                                <span>기간</span>
+                                                <span class="bubble-item bubble">${period}</span>
+                                            </div>
+
+                                            <div class="content">
+                                                <span>모임 방식</span>
+                                                <span class="bubble-item bubble">${meetingType}</span>
+                                            </div>
+
+                                            <div class="content">
+                                            <span>모집 현황</span>
+                                            <span id="recruitmentState" class="bubble-item ${recruitmentStateColor}">${recruitmentState}</span>
+                                        </div>
+
+                                        </div>
+                                        <div class="card-content-box">
+                                        <div>
+                                            <div class="content">
+                                                <i class="fa-regular fa-eye"></i>
+                                                <span>${hits}</span>
+                                            </div>
+                                        </div>
+                                        </div>
+                                    </div>
+                                </div>`;
+
+                $('#profile-section').append(tempHTML);
+            }
+            // resizeHomeContainer();
+        }
+    });
+}
+
+window.readMyComments = () => {
+    $('#readMyComments').attr('class', 'is-active');
+    $('#readMyPosts').attr('class', '');
+    $('#profile-section').empty();
+
+    let token = Cookies.get('token');
+    $.ajax({
+        type: 'GET',
+        url: process.env.BACKEND_HOST + '/profile/posts',
+        beforeSend: function (xhr) {
+            xhr.setRequestHeader('Content-type', 'application/json');
+            xhr.setRequestHeader('Authorization', 'Bearer ' + token);
+        },
+        success: function (response) {
+            let posts = response;
+
+            for (let index = 0; index < posts.length; index++) {
+                let id = posts[index]['id'];
+                let title = posts[index]['title'];
+                let meetingType = posts[index]['meetingType'];
+                let period = posts[index]['period'];
+                let hits = posts[index]['hits'];
+                let recruitmentState = posts[index]['recruitmentState'] ? '모집 완료' : '모집 중';
+
+                let recruitmentStateColor = posts[index]['recruitmentState'] ? 'is-default' : 'is-pink';
+                let recruitmentStateColorBack = posts[index]['recruitmentState'] ? 'is-white' : 'is-gray';
+
+                let tempHTML = `<div id=${id} class="card ${recruitmentStateColorBack}">
+                                    <div class="card-header">
+                                        <p class="card-header-title" onclick="openPost(${id})" >${title}</p>
+                                        <div onclick="deleteBookmark(${id})">
+                                            <button class="delete"></button>
+                                        </div>
+                                    </div>
+
+                                    <div class="card-content" onclick="openPost(${id})">
+                                        <div class="card-content-box">
+                                            <div class="content">
+                                                <span>기간</span>
+                                                <span class="bubble-item bubble">${period}</span>
+                                            </div>
+
+                                            <div class="content">
+                                                <span>모임 방식</span>
+                                                <span class="bubble-item bubble">${meetingType}</span>
+                                            </div>
+
+                                            <div class="content">
+                                            <span>모집 현황</span>
+                                            <span id="recruitmentState" class="bubble-item ${recruitmentStateColor}">${recruitmentState}</span>
+                                        </div>
+
+                                        </div>
+                                        <div class="card-content-box">
+                                        <div>
+                                            <div class="content">
+                                                <i class="fa-regular fa-eye"></i>
+                                                <span>${hits}</span>
+                                            </div>
+                                        </div>
+                                        </div>
+                                    </div>
+                                </div>`;
+
+                $('#profile-section').append(tempHTML);
+            }
+            // resizeHomeContainer();
+        }
+    });
+}
