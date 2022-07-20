0f4f2b2f5024701e1aa820378b40632ace88b686
rimsong <kariber21@naver.com>
2022-07-21 00:21:34 +0900

    Fix: re-fix Bugs

--- src/js/editor.js
+++ src/js/editor.js
@@ -63,9 +63,11 @@ window.registerPost = () => {
         error: function (response) {
             if (response.status == 403) {
                 window.openLoginModal();
-            } else if (response.status == 400) {
+            }
+            else if (response.status == 400) {
                 alert('본문은 2000자 이내로 작성해주세요.');
-            } else {
+            }
+            else {
                 alert('글 저장에 실패하였습니다!');
             }
             console.log(response);
