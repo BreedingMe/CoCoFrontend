df9a7128783f4770a392dd229d8cabf55a916eac
rimsong <kariber21@naver.com>
2022-07-27 23:13:51 +0900

    Feat: Create Filter

--- src/js/home.js
+++ src/js/home.js
@@ -28,14 +28,31 @@ window.initializeHome = () => {
     }
 };
 
+// 모집 중 / 모집 마감 버튼 클릭에 따른 이벤트
 window.recruitmentStateCheckbox = () => {
     localStorage.setItem('check', $('#recruitmentStateCheckbox').is(':checked'));
-
+    let value = $('#sort-post option:selected').val();
     if ($('input:checkbox[id="recruitmentStateCheckbox"]').is(':checked')) {
-        getPosts();
+        if (value == '1') {
+            getPosts();
+        }
+        else if (value == '2') {
+            sortHitsPost();
+        }
+        else if (value == '3') {
+            sortCommentsSizePost();
+        }
     }
     else {
-        getRecrutingPosts();
+        if (value == '1') {
+            getRecrutingPosts();
+        }
+        else if (value == '2') {
+            sortRecruitingHitsPost();
+        }
+        else if (value == '3') {
+            sortRecruitingCommentsSizePost();
+        }
     }
 };
 
@@ -56,6 +73,294 @@ function resizeHomeContainer() {
         homeContainer.css('height', '');
     }
 }
+
+// 필터 (Select) 변경 감지
+window.sortPost = () => {
+    $('#home-section-post').empty();
+    let value = $('#sort-post option:selected').val();
+    if ($('input:checkbox[id="recruitmentStateCheckbox"]').is(':checked')) {
+        if (value == '1') {
+            getPosts();
+        }
+        else if (value == '2') {
+            sortHitsPost();
+        }
+        else if (value == '3') {
+            sortCommentsSizePost();
+        }
+    }
+    else {
+        if (value == '1') {
+            getRecrutingPosts();
+        }
+        else if (value == '2') {
+            sortRecruitingHitsPost();
+        }
+        else if (value == '3') {
+            sortRecruitingCommentsSizePost();
+        }
+    }
+};
+
+// 인기순 정렬 (모집 중인거만)
+window.sortRecruitingHitsPost = () => {
+    $('#home-section-post').empty();
+    $.ajax({
+        type: 'GET',
+        url: process.env.BACKEND_HOST + '/post/list/recruit/hits',
+        success: function (response) {
+            console.log(response);
+            let posts = response;
+
+            for (let index = 0; index < posts.length; index++) {
+                let id = posts[index]['id'];
+                let title = posts[index]['title'];
+                let meetingType = posts[index]['meetingType'];
+                let period = posts[index]['period'];
+                let hits = posts[index]['hits'];
+                let recruitmentState = posts[index]['recruitmentState'] ? '모집 완료' : '모집 중';
+
+                let recruitmentStateColor = posts[index]['recruitmentState'] ? 'is-default' : 'is-pink';
+                let recruitmentStateColorBack = posts[index]['recruitmentState'] ? 'is-white' : 'is-gray';
+
+                let cardHTML = `<div id=${id} class="card ${recruitmentStateColorBack}">
+                                    <div class="card-header">
+                                        <p class="card-header-title" onclick="openPost(${id})">${title}</p>
+                                        <div class="content bookmark">
+                                            <i class="fa-regular fa-bookmark" onclick="registerBookmark(${id})"></i>
+                                        </div>
+                                    </div>
+
+                                    <div class="card-content" onclick="openPost(${id})">
+                                        <div class="card-content-box">
+                                            <div class="content">
+                                                <span>기간</span>
+                                                <span class="bubble-item bubble">${period}</span>
+                                            </div>
+
+                                            <div class="content">
+                                                <span>모임 방식</span>
+                                                <span class="bubble-item bubble">${meetingType}</span>
+                                            </div>
+
+                                            <div class="content">
+                                            <span>모집 현황</span>
+                                            <span class="bubble-item ${recruitmentStateColor}">${recruitmentState}</span>
+                                        </div>
+
+                                        </div>
+                                        <div class="card-content-box">
+                                        <div>
+                                            <div class="content">
+                                                <i class="fa-regular fa-eye"></i>
+                                                <span>${hits}</span>
+                                            </div>
+                                        </div>
+                                        </div>
+                                    </div>
+                                </div>`;
+
+                $('#home-section-post').append(cardHTML);
+            }
+            resizeHomeContainer();
+        }
+    });
+};
+
+// 인기순 정렬 (모집 완료 포함)
+window.sortHitsPost = () => {
+    // let check = localStorage.getItem('check');
+    $('#home-section-post').empty();
+    $.ajax({
+        type: 'GET',
+        url: process.env.BACKEND_HOST + '/post/list/hits',
+        success: function (response) {
+            console.log(response);
+            let posts = response;
+
+            for (let index = 0; index < posts.length; index++) {
+                let id = posts[index]['id'];
+                let title = posts[index]['title'];
+                let meetingType = posts[index]['meetingType'];
+                let period = posts[index]['period'];
+                let hits = posts[index]['hits'];
+                let recruitmentState = posts[index]['recruitmentState'] ? '모집 완료' : '모집 중';
+
+                let recruitmentStateColor = posts[index]['recruitmentState'] ? 'is-default' : 'is-pink';
+                let recruitmentStateColorBack = posts[index]['recruitmentState'] ? 'is-white' : 'is-gray';
+
+                let cardHTML = `<div id=${id} class="card ${recruitmentStateColorBack}">
+                                    <div class="card-header">
+                                        <p class="card-header-title" onclick="openPost(${id})">${title}</p>
+                                        <div class="content bookmark">
+                                            <i class="fa-regular fa-bookmark" onclick="registerBookmark(${id})"></i>
+                                        </div>
+                                    </div>
+
+                                    <div class="card-content" onclick="openPost(${id})">
+                                        <div class="card-content-box">
+                                            <div class="content">
+                                                <span>기간</span>
+                                                <span class="bubble-item bubble">${period}</span>
+                                            </div>
+
+                                            <div class="content">
+                                                <span>모임 방식</span>
+                                                <span class="bubble-item bubble">${meetingType}</span>
+                                            </div>
+
+                                            <div class="content">
+                                            <span>모집 현황</span>
+                                            <span class="bubble-item ${recruitmentStateColor}">${recruitmentState}</span>
+                                        </div>
+
+                                        </div>
+                                        <div class="card-content-box">
+                                        <div>
+                                            <div class="content">
+                                                <i class="fa-regular fa-eye"></i>
+                                                <span>${hits}</span>
+                                            </div>
+                                        </div>
+                                        </div>
+                                    </div>
+                                </div>`;
+
+                $('#home-section-post').append(cardHTML);
+            }
+            resizeHomeContainer();
+        }
+    });
+};
+// 댓글수순 정렬  (모집 중인거만)
+window.sortRecruitingCommentsSizePost = () => {
+    $('#home-section-post').empty();
+    $.ajax({
+        type: 'GET',
+        url: process.env.BACKEND_HOST + '/post/list/recruit/comments',
+        success: function (response) {
+            console.log(response);
+            let posts = response;
+
+            for (let index = 0; index < posts.length; index++) {
+                let id = posts[index]['id'];
+                let title = posts[index]['title'];
+                let meetingType = posts[index]['meetingType'];
+                let period = posts[index]['period'];
+                let hits = posts[index]['hits'];
+                let recruitmentState = posts[index]['recruitmentState'] ? '모집 완료' : '모집 중';
+
+                let recruitmentStateColor = posts[index]['recruitmentState'] ? 'is-default' : 'is-pink';
+                let recruitmentStateColorBack = posts[index]['recruitmentState'] ? 'is-white' : 'is-gray';
+
+                let cardHTML = `<div id=${id} class="card ${recruitmentStateColorBack}">
+                                    <div class="card-header">
+                                        <p class="card-header-title" onclick="openPost(${id})">${title}</p>
+                                        <div class="content bookmark">
+                                            <i class="fa-regular fa-bookmark" onclick="registerBookmark(${id})"></i>
+                                        </div>
+                                    </div>
+
+                                    <div class="card-content" onclick="openPost(${id})">
+                                        <div class="card-content-box">
+                                            <div class="content">
+                                                <span>기간</span>
+                                                <span class="bubble-item bubble">${period}</span>
+                                            </div>
+
+                                            <div class="content">
+                                                <span>모임 방식</span>
+                                                <span class="bubble-item bubble">${meetingType}</span>
+                                            </div>
+
+                                            <div class="content">
+                                            <span>모집 현황</span>
+                                            <span class="bubble-item ${recruitmentStateColor}">${recruitmentState}</span>
+                                        </div>
+
+                                        </div>
+                                        <div class="card-content-box">
+                                        <div>
+                                            <div class="content">
+                                                <i class="fa-regular fa-eye"></i>
+                                                <span>${hits}</span>
+                                            </div>
+                                        </div>
+                                        </div>
+                                    </div>
+                                </div>`;
+
+                $('#home-section-post').append(cardHTML);
+            }
+            resizeHomeContainer();
+        }
+    });
+};
+
+// 댓글수순 정렬  (모집 완료 포함)
+window.sortCommentsSizePost = () => {
+    $('#home-section-post').empty();
+    $.ajax({
+        type: 'GET',
+        url: process.env.BACKEND_HOST + '/post/list/comments',
+        success: function (response) {
+            console.log(response);
+            let posts = response;
+
+            for (let index = 0; index < posts.length; index++) {
+                let id = posts[index]['id'];
+                let title = posts[index]['title'];
+                let meetingType = posts[index]['meetingType'];
+                let period = posts[index]['period'];
+                let hits = posts[index]['hits'];
+                let recruitmentState = posts[index]['recruitmentState'] ? '모집 완료' : '모집 중';
+
+                let recruitmentStateColor = posts[index]['recruitmentState'] ? 'is-default' : 'is-pink';
+                let recruitmentStateColorBack = posts[index]['recruitmentState'] ? 'is-white' : 'is-gray';
+
+                let cardHTML = `<div id=${id} class="card ${recruitmentStateColorBack}">
+                                    <div class="card-header">
+                                        <p class="card-header-title" onclick="openPost(${id})">${title}</p>
+                                        <div class="content bookmark">
+                                            <i class="fa-regular fa-bookmark" onclick="registerBookmark(${id})"></i>
+                                        </div>
+                                    </div>
+
+                                    <div class="card-content" onclick="openPost(${id})">
+                                        <div class="card-content-box">
+                                            <div class="content">
+                                                <span>기간</span>
+                                                <span class="bubble-item bubble">${period}</span>
+                                            </div>
+
+                                            <div class="content">
+                                                <span>모임 방식</span>
+                                                <span class="bubble-item bubble">${meetingType}</span>
+                                            </div>
+
+                                            <div class="content">
+                                            <span>모집 현황</span>
+                                            <span class="bubble-item ${recruitmentStateColor}">${recruitmentState}</span>
+                                        </div>
+
+                                        </div>
+                                        <div class="card-content-box">
+                                        <div>
+                                            <div class="content">
+                                                <i class="fa-regular fa-eye"></i>
+                                                <span>${hits}</span>
+                                            </div>
+                                        </div>
+                                        </div>
+                                    </div>
+                                </div>`;
+
+                $('#home-section-post').append(cardHTML);
+            }
+            resizeHomeContainer();
+        }
+    });
+};
 
 // 검색
 window.searchPost = () => {
@@ -127,7 +432,6 @@ window.searchPost = () => {
         }
     });
 };
-
 /* AJAX */
 // 모집 중인 게시글만 (default)
 function getRecrutingPosts() {
@@ -135,7 +439,7 @@ function getRecrutingPosts() {
 
     $.ajax({
         type: 'GET',
-        url: process.env.BACKEND_HOST + '/post/recruit/list',
+        url: process.env.BACKEND_HOST + '/post/list/recruit',
         success: function (response) {
             let posts = response;
 
@@ -196,6 +500,7 @@ function getRecrutingPosts() {
 
 // 모집 중 & 모집완료 게시글 모두
 function getPosts() {
+    // let check = localStorage.getItem('check');
     $('#home-section-post').empty();
 
     $.ajax({
